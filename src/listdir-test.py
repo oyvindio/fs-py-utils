6c514f342474a5d6aa8a4d2169eca2ea99f137a8
Øyvind Øvergaard <oyvind.overgaard@gmail.com>
2010-02-08 00:36:54 +0100

    added shelve "caching"

--- src/listdir-test.py
+++ src/listdir-test.py
@@ -4,57 +4,62 @@
 import os
 import timeit
 import random
+import shelve
 import numpy as np
+import matplotlib as mpl
+mpl.use("svg") # plot graphs as scalable vector graphics
+import matplotlib.pyplot as plt
 
-ROOT_DIR = "/tmp"
-DIRS_TO_TEST = 10
 
-class RandomListdir(object):
+ROOT_DIR = "/"
+SHELVE_FILE = "dirs.db"
+
+def findAllDirs(directory):
     """
+    Returns a list of all subdirectories under directory
+
+    Arguments:
+    - `directory`: the directory from which we start the os.walk call
     """
-    
-    def __init__(self, root="/"):
-        """
-        """
-        self.root = root
-        self.directories = self._findAllDirs(root)
+    # this effectively calls os.listdir on each directory under directory,
+    # which may affect measurements... (caching etc.)
+    return [root for root, dirs, files in os.walk(directory)]
 
-    def _findAllDirs(self, directory):
-        """
-        Returns a list of all subdirectories under directory
+def shelveDirs(directories, filename=SHELVE_FILE):
+    """
+    Writes a list of all paths on the system to a shelve at `filename`.
+    """
+    s = shelve.open(filename)
+    s["dirs"] = directories
+    s.close()
 
-        Arguments:
-        - `directory`: the directory from which we start the os.walk call
-        """
-        # this effectively calls os.listdir on each directory under directory,
-        # which may affect measurements... (caching etc.)
-        return [root for root, dirs, files in os.walk(directory)]
-
-    def testDir(self, directory):
-        """
-        Arguments:
-        - `directory`: the directory we want to test
-        """
-        return os.listdir(directory)
-
-    def randomDir(self):
-        """
-        returns a random directory
-        """
-        return random.choice(self.directories)
-
+def readShelvedDirs(filename=SHELVE_FILE):
+    """
+    Tries to read a list of paths from the shelve at `filename`. If no
+    list is found, scans the file system, creates a new shelve and
+    returns a new list of directories.
+    """
+    s = shelve.open(filename)
+    if not s.has_key("dirs"):
+        directories = findAllDirs(ROOT_DIR)
+        shelveDirs(directories)
+        return directories
+    else:
+        directories = s["dirs"]
+        s.close()
+        return directories
 
 if __name__ == "__main__":
+    directories = readShelvedDirs()
     statement = """\
-r.testDir(d)
-    """
+os.listdir(d)
+"""
     setup="""\
 import os
 import random
-from __main__ import RandomListdir
-r = RandomListdir('/tmp')
-d = r.randomDir()
-    """
-    t = timeit.Timer(statement, setup)
-    runtimes = t.repeat(10)
-    print runtimes
+d = random.choice({0})
+print d
+""".format(directories)
+    runtime  = timeit.timeit(statement, setup, number=1)
+    print runtime
+
