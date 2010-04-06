525829377c125fc43ac778c9e9f2fd6e84ca793b
Øyvind Øvergaard <oyvind.overgaard@gmail.com>
2010-04-06 16:17:01 +0200

    python 2.5 compatibility + renamed a few variables

--- src/memmap-test.py
+++ src/memmap-test.py
@@ -15,21 +15,20 @@ def genSample(root=ROOT_DIR):
     files = filter(os.path.isfile, p.allPaths())
     statement = "np.memmap(f, dtype=ext)"
     while True:
-        mmapFile = random.choice(files)
-        setup = "import os; import numpy as np; f = {0}, ext = {1}".format(
-            mmapFile, dtypeFromExt(mmapFile))
+        filename = random.choice(files)
+        setup = "import os; import numpy as np; f = %s, ext = %s" % filename, dtypeFromExt(filename)
         t = timeit.Timer(statement, setup)
         try:
             runtime = t.timeit(1)
-            yield (mmapFile, runtime)
+            yield (filename, runtime)
         except OSError:
             pass
 
-def dtypeFromExt(mmapFile):
+def dtypeFromExt(filename):
     """
+    returns the dtype to pass to numpy.memmap based on filename
     """
-    (filename, ext) = os.path.splitext(mmapFile)
-    
+    (filename, ext) = os.path.splitext(filename)
     return ext[1:] # strip leading . from ext
 
 if __name__ == "__main__":
